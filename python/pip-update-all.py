d0833eb44c913566ee6562961695c35bc2156f84
amirmazmi <amirmazmi.bb@gmail.com>
2017-04-20 15:04:55 +1000

    Add files via upload

---
+++ python/pip-update-all.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+
+import pip
+from subprocess import call
+ 
+for dist in pip.get_installed_distributions():
+	print("\n")
+	print("-------------------------------------------------")
+	print("----- I should have installed conda!!! ----------")
+	print("-------------------------------------------------")
+	print("\n")
+	call("sudo pip3 install --upgrade " + dist.project_name, shell=True)
+
+
+print(" ####### DONE ########")
