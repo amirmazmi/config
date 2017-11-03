bc08d07beed4be8fc0db3e3ebb38cdf2b3ee2ca4
amirmazmi <amirmazmi@users.noreply.github.com>
2017-11-03 13:52:34 +1100

    version check template

---
+++ python/Vercheck.py
@@ -0,0 +1,23 @@
+ # -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import nltk
+import sklearn
+import seaborn
+
+print('The nltk version is {}.'.format(nltk.__version__))
+print('The scikit-learn version is {}.'.format(sklearn.__version__))
+print('The seaborn version is {}.'.format(seaborn.__version__))
+
+#The nltk version is 3.1.
+#The scikit-learn version is 0.17.
+#The seaborn version is 0.7.0.
+
+
+
+
+
