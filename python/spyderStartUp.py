16e87f0405f73c44d0ed827f6506cf2e3f47bdc8
amirmazmi <amirmazmi@users.noreply.github.com>
2018-05-21 12:37:35 +0800

    Add files via upload
    new file to edit pandas options for display width and max columns

---
+++ python/spyderStartUp.py
@@ -0,0 +1,21 @@
+
+print('\n\n\t--- Startup script ---')
+# ------------------------------------------------
+# change pandas options
+import pandas as pd
+print('\n[+] Adjusting pandas options ',
+		'\n\t display.width 		{} -> {}'.format( pd.get_option('display.width'), 150),
+		'\n\t display.max_column 	{} -> {}'.format( pd.get_option('display.max_column'), 50)
+		)
+pd.set_option('display.max_column', 50)
+pd.set_option('display.width', 150)
+del pd
+
+# ------------------------------------------------
+# add new section here
+
+
+
+# ------------------------------------------------
+print('\n\n\t--- End of script ---\n\n')
+
