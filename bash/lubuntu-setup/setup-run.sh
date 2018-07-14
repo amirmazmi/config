f74c02c136069ac3f90195135289de250be643af
amirmazmi <amirmazmi@users.noreply.github.com>
2018-07-14 13:51:55 +0800

    Create setup-run.sh

---
+++ bash/lubuntu-setup/setup-run.sh
@@ -0,0 +1,23 @@
+# script to execute lubuntu setup
+#   1. append alias to bashrc - backup previous file
+#   2. edit lxterminal.conf - backup previous file
+#   3. install linux packages
+#   4. edit grub file for fn key
+
+# 1. append alias to bashrc - backup previous file
+python3 bashrc-update
+
+# 2. edit lxterminal.conf - backup previous file
+# back up file
+# copy file in
+
+
+# 3. install linux packages
+xargs -a linux-packages sudo apt install 
+
+
+#   4. edit grub file for fn key
+
+
+
+
