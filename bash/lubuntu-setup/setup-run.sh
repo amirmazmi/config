5313888cdfa8458d5d5bd3cdeea89e763eab8fa3
amirmazmi <amirmazmi@users.noreply.github.com>
2018-07-14 20:55:21 +0800

    Add files via upload

--- bash/lubuntu-setup/setup-run.sh
+++ bash/lubuntu-setup/setup-run.sh
@@ -1,26 +1,67 @@
+#!/bin/bash
+
 # script to execute lubuntu setup
 #   1. append alias to bashrc - backup previous file
 #   2. edit lxterminal.conf - backup previous file
 #   3. install linux packages
 #   4. edit grub file for fn key
 
+# get user inputs 
+read -p "	Append update alias to bashrc? (y/n)  " append
+if [ "$append" != "y" ] && [ "$append" != "n" ]
+then 
+	echo "Thats not an option!!!"
+	exit 1
+fi
+
+read -p "	Change lxterminal config? (y/n)  " lxterm
+if [ "$lxterm" != "y" ] && [ "$lxterm" != "n" ]
+then 
+	echo "Thats not an option!!!"
+	exit 1
+fi
+read -p "	Install packages? (y/n)  " instpack
+if [ "$instpack" != "y" ] && [ "$instpack" != "n" ]
+then 
+	echo "Thats not an option!!!"
+	exit 1
+fi
+
+# ----------------------------------------------------------------
+# Execute necessary commands
+
 # 1. append alias to bashrc - backup previous file
-lxterminal -e 'bash -c "python3 bashrc-update; bash"'
+if [[ $append = "y" ]]
+then
+	lxterminal -e 'bash -c "python3 bashrc-update.py; sleep 10"'
+fi 
 
 # 2. edit lxterminal.conf - backup previous file
-# back up file
-mv -vvv ~/.config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf-backup
-# copy file in
-mv -vvvv lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+if [[ $lxterm = "y" ]]
+then
+	echo
+	# back up file
+	sudo mv -vvv ~/.config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf-backup
+	# copy file in
+	sudo mv -vvvv lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+	echo
+fi
 
-# 3. install linux packages
-lxterminal -e 'bash -c "xargs -a linux-packages sudo apt install; bash"'
-
+if [[ $instpack = "y" ]] 
+then
+	# 3. install linux packages
+	lxterminal -e 'bash -c "xargs -a linux-packages sudo apt install; sleep 10"'
+fi
 
 #   4. edit grub file for fn key
 
-
+echo
+echo
 sleep 5
-lxterminal -e "bash"
+echo
+echo	### DONE! ###
+echo
+echo
+
 exit
 
