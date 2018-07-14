73b0f5da1dc709616cea098de53f1e2ab3c68b74
amirmazmi <amirmazmi@users.noreply.github.com>
2018-07-14 18:39:47 +0800

    Update setup-run.sh

--- bash/lubuntu-setup/setup-run.sh
+++ bash/lubuntu-setup/setup-run.sh
@@ -9,15 +9,18 @@ lxterminal -e 'bash -c "python3 bashrc-update; bash"'
 
 # 2. edit lxterminal.conf - backup previous file
 # back up file
+mv -vvv ~/.config/lxterminal/lxterminal.conf ~/.config/lxterminal/lxterminal.conf-backup
 # copy file in
-
+mv -vvvv lxterminal.conf ~/.config/lxterminal/lxterminal.conf
 
 # 3. install linux packages
-lxterminal -e 'bash -c "xargs -a linux-packages sudo apt install"'
+lxterminal -e 'bash -c "xargs -a linux-packages sudo apt install; bash"'
 
 
 #   4. edit grub file for fn key
 
 
-
+sleep 5
+lxterminal -e "bash"
+exit
 
