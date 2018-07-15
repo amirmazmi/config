777206fd0cf3335d26428078f44174464ba42e7d
amirmazmi <amirmazmi@users.noreply.github.com>
2018-07-15 15:44:11 +1000

    Update setup-run.sh

--- bash/lubuntu-setup/setup-run.sh
+++ bash/lubuntu-setup/setup-run.sh
@@ -4,7 +4,8 @@
 #   1. append alias to bashrc - backup previous file
 #   2. edit lxterminal.conf - backup previous file
 #   3. install linux packages
-#   4. edit grub file for fn key
+#   4. edit lubuntu-rc.xml for shortcuts
+#   5. edit grub file for fn key
 
 # get user inputs 
 read -p "	Append update alias to bashrc? (y/n)  " append
@@ -26,8 +27,17 @@ then
 	echo "Thats not an option!!!"
 	exit 1
 fi
-echo
 
+read -p "	Change lubuntu-rc.xml? (y/n)   " lubrc
+if [ "$lubrc" != "y" ] && [ "$lubrc" != "n" ]
+then 
+	echo "Thats not an option!!!"
+	exit 1
+fi
+
+
+# ----------------------------------------------------------------
+echo
 # ----------------------------------------------------------------
 # Execute necessary commands
 
@@ -54,7 +64,21 @@ then
 	nohup lxterminal -e 'bash -c "xargs -a linux-packages sudo apt install; sleep 20"' $ 2>/dev/null
 fi
 
-#   4. edit grub file for fn key
+#   4. edit lubuntu-rc.xml for shortcuts
+if [[ $lubrc = "y" ]] 
+then
+	echo
+	# backup file
+	mv -vvv ~/.config/openbox/lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml-backup
+	# copy file in 
+	cp -vvv lubuntu-rc.xml ~/.config/openbox/lubuntu-rc.xml
+	echo
+fi
+
+#   5. edit grub file for fn key
+
+
+
 
 printf "\n\n "
 printf "\t\t### DONE! ###"
