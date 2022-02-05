b2b7461011d7b76d04335661a14654f48aac882b
amirmazmi <amirmazmi@users.noreply.github.com>
2022-02-05 14:22:46 +0800

    Add files via upload

---
+++ bash/monitors_startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#  Startup script to check for connected displays and change if:
+# 	2 monitors connected
+#
+#----------------------------------------------------------------
+# add the following line to /.profile
+# @bash <location of file>
+#
+
+
+sleep 2
+
+moncount=$(xrandr | grep ' connected' | grep -o '^[A-Z]*-[0-9]*\w' | wc -l )
+
+if [ $moncount -eq 2 ]
+then
+    xrandr --output HDMI-1 --mode 2560x1080 --pos 0x320 --rotate normal --output DP-1 --mode 1920x1080 --pos 2560x0 --rotate left --output eDP-1 --off --output DP-2 --off
+    lxpanelctl restart
+
+fi
