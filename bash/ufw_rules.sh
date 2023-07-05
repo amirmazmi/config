55ea476322ed09baa69444c9fd85f0a074649840
amirmazmi <amirmazmi@users.noreply.github.com>
2023-07-06 01:59:13 +0800

    added comment tag

--- bash/ufw_rules.sh
+++ bash/ufw_rules.sh
@@ -18,19 +18,19 @@ echo -e "\n\t UFW: Apply default rule deny incoming\n"
 sudo ufw default deny incoming
 
 echo -e "\n\t UFW: Applying rule - SSH\n"
-sudo ufw limit in from 192.168.1.0/24 to any port 22222 proto tcp
+sudo ufw limit in from 192.168.1.0/24 to any port 22222 proto tcp comment 'ssh'
 
 echo -e "\n\t UFW: Applying rule - BACnet\n"
-sudo ufw allow in from any to any port 47808 proto udp
+sudo ufw allow in from any to any port 47808 proto udp comment 'BACnet'
 
 echo -e "\n\t UFW: Applying rule - VNC\n"
-sudo ufw limit in from 192.168.1.0/24 to any port 5901
-sudo ufw limit in from 192.168.1.0/24 to any port 5902
+sudo ufw limit in from 192.168.1.0/24 to any port 5901 comment 'VNC'
+sudo ufw limit in from 192.168.1.0/24 to any port 5902 comment 'VNC'
 
 echo -e "\n\t UFW: Applying rule - block SMB (double pulsar)\n"
-sudo ufw deny 137
-sudo ufw deny 139
-sudo ufw deny 445
+sudo ufw deny 137 comment 'block double pulsar'
+sudo ufw deny 139 comment 'block double pulsar'
+sudo ufw deny 445 comment 'block double pulsar'
 
 sudo ufw reload
 
